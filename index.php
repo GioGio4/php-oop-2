9803bc8f93dbde6cfde05c65ad28496fe36c282e
GioGio4 <g.ceccobelli@outlook.it>
2023-03-23 17:24:57 +0100

    Add initial class Product,Categories and Cibo (Cibo eredita>Prodotto)

--- index.php
+++ index.php
@@ -6,12 +6,76 @@ Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
 
 <?php
 
+class Product
+{
+    // Variabili di istanza della classe Product 
+    public $picture;
+    public $title;
+    public $price;
+    public $categories;
+
+
+    public function __construct(
+        string $_picture,
+        string $_title,
+        float $_price,
+        Categories $_categories
+    ) {
+        $this->picture = $_picture;
+        $this->title = $_title;
+        $this->price = $_price;
+        $this->categories = $_categories;
+    }
+}
+
+class Categories
+{
+    // Variabili di istanza della classe Categories
+    public $name_categories;
+    public $icon;
+
+    public function __construct($_name_categories, $_icon)
+    {
+        $this->name_categories = $_name_categories;
+        $this->icon = $_icon;
+    }
+}
 
 
 
+$categories_dog = new Categories("Cane", "ICON-CANE");
+$categories_cat = new Categories("Gatto", "ICON-GATTO");
 
 
+// !!PROVA!!
+// $prodotto_1 = new Product("Foto", "cibo cane", 3.10, $categories_dog);
 
+// var_dump($prodotto_1);
+
+class Cibo extends Product
+{
+    // Variabili di istanza della classe Categories che eredità variabili da (Product)
+    public $scadenza;
+    public $gusto;
+
+    public function __construct(
+        string $_picture,
+        string $_title,
+        float $_price,
+        Categories $_categories,
+        string $_scadenza,
+        string $_gusto
+    ) {
+        parent::__construct($_picture, $_title, $_price, $_categories);
+        $this->scadenza = $_scadenza;
+        $this->gusto = $_gusto;
+    }
+}
+
+// !!PROVA!! 
+$carneManzo = new Cibo("FOTO-PRODOTTO", "Carne di Manzo", 7.99, $categories_dog, "01/01/2025", "Manzo");
+
+var_dump($carneManzo);
 
 
 ?>
